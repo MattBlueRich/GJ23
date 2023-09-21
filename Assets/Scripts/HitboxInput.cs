7c37db30b76ed164a584b7d17d69653fd44fadcb
Richard-Casey <s233122@uos.ac.uk>
2023-09-21 12:18:12 +0100

    Morning coding

--- Assets/Scripts/HitboxInput.cs
+++ Assets/Scripts/HitboxInput.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class HitboxInput : MonoBehaviour
 {
 
-    bool canPress;
+    bool canPressdot;
+    bool canPressDash;
+    bool holdReady = false;
     GameObject dot;
+    GameObject Dash;
+    public float holdTime = 0.75f;
+    float downTime, upTime, pressTime = 0;
+
 
 
     // Start is called before the first frame update
@@ -16,28 +22,82 @@ public class HitboxInput : MonoBehaviour
     }
 
     // Update is called once per frame
+    // Checking if dot is in the hitbox & with space press
+    // canPress is true when anything enters hitbox
     void Update()
     {
-        if(Input.GetKeyDown("space") && canPress)
+        if(Input.GetKeyDown("space") && canPressdot)
         {
             Debug.Log("DESTROY");
-            Destroy(dot);
+            dot.GetComponent<Animator>().SetTrigger("Expand");
+            StartCoroutine(HitAnimation(1f, dot));
         }
+
+        if (Input.GetKeyDown("space") && canPressDash && holdReady == false)
+        {
+            downTime = Time.time;
+
+            pressTime = downTime + holdTime;
+
+            holdReady = true;
+              
+        }
+
+        if (Input.GetKeyUp("space"))
+        {
+            holdReady = false;
+        }
+       
+        if (Time.time >= pressTime && holdReady == true)
+        {
+            holdReady = false;
+            Destroy(Dash);
+        }
+       
+       
+  
+
+    
     }
 
+    // Detecting trigger collision in hitbox
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        canPress = true;
+       
         if (collision.gameObject.CompareTag("Dot"))
-        {
+        { 
+            canPressdot = true;
             Debug.Log("DOT DETECTED");
             dot = collision.gameObject;
         }
+
+        if (collision.gameObject.CompareTag("Dash"))
+        {
+            canPressDash = true;
+            Debug.Log("DASH DETECTED");
+            Dash = collision.gameObject;
+        }
     }
 
+    // Detecting dot leaving hitbox collision
     private void OnTriggerExit2D(Collider2D collision)
     {
-        Debug.Log("DOT LEFT");
-        canPress = false;
+        if (collision.gameObject.CompareTag("Dot"))
+        {
+            canPressdot = false;
+            Debug.Log("DOT LEFT");
+        }
+
+        if (collision.gameObject.CompareTag("Dash"))
+        {
+            canPressDash = false;
+            Debug.Log("DASH LEFT");
+        }
+    }
+    IEnumerator HitAnimation(float AnimationTime, GameObject Object)
+    {
+        yield return new WaitForSeconds(AnimationTime);
+        Destroy(Object);
+
     }
 }
